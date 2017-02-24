2788e4363a237baad2d6a28aed4cdd3c6998247b
Aleksandra <aleksandra.vercauteren@gmail.com>
2017-02-24 16:57:59 +0100

    added python script for Q5

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,19 @@
-# Hint:  use Google to find python function
+import datetime
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
+new_start = datetime.datetime.strptime(date_start, "%m-%d-%Y")
+new_end = datetime.datetime.strptime(date_stop, "%m-%d-%Y")
+print new_end - new_start
 
-####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015'
+new_start = datetime.datetime.strptime(date_start, "%m%d%Y")
+new_end = datetime.datetime.strptime(date_stop, "%m%d%Y")
+print new_end - new_start
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+new_start = datetime.datetime.strptime(date_start, "%d-%b-%Y")
+new_end = datetime.datetime.strptime(date_stop, "%d-%b-%Y")
+print new_end - new_start

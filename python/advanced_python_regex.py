169c669074eb3058cafdae77ba26c4ea30a4e3bf
Aleksandra <aleksandra.vercauteren@gmail.com>
2017-02-27 17:11:02 +0100

    Script for Part I advanced python

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,33 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+import re
+
+with open("faculty.csv") as f:
+    data = pd.read_csv(f)
+    data = data.rename(columns=lambda x: x.strip())
+    print data.head()
+    degrees = data['degree'].str.strip()
+    degrees = degrees.str.replace('\.', '')
+    df = degrees.str.split(' ', expand=True)
+    values = df.apply(pd.Series.value_counts)
+    sum_degrees = values.sum(axis = 1)
+    print 'There are '+str(len(sum_degrees)) + ' different degrees.\n'
+    for degree, total in sum_degrees.iteritems():
+        # print degree
+        print 'There are ' + str(int(total)) +' ' +degree+'s.'
+
+    titles = data['title'].str.strip()
+    df = titles.str.split(' ', expand=True)
+    sum_titles = df[0].value_counts()
+    print '\nThere are '+str(len(sum_titles)) + ' different titles.\n'
+    for title, total in sum_titles.iteritems():
+        if title == 'Professor':
+            print 'There are ' + str(int(total)) +' ' +title+'s of Biostatistics.'
+        else:
+            print 'There are ' + str(int(total)) +' ' +title+' Professors of Biostatistics.'
+
+    emails = data['email'].str.strip()
+    print list(emails)
+
+    domains = emails.str.split('@', expand = True)
+    print 'There are ' + str(len(domains[1].unique())) + ' domains :' + str(domains[1].unique())
+
